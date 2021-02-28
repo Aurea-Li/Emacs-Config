43b20befb68754c7d615adb667a9fe8e4ca6b781
Aurea Li <aurea@squareup.com>
2021-02-28 10:21:14 -0800

    Update config

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;; Installing MELPA
+ ;; Installing MELPA
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
@@ -20,7 +20,9 @@
 (global-set-key (kbd "C-c c") 'org-capture)
 ;; Set agenda files to be all .org files under directory
 ;; ~/Notes/Org-Mode recursively.
-(setq org-agenda-files (directory-files-recursively "~/Notes/Org-Mode/" "\.org$"))
+(setq org-agenda-files (directory-files-recursively "~/Notes/" "\.org$"))
+;; Set agenda to be 12 hours instead of 24.
+(setq org-agenda-timegrid-use-ampm 1)
 ;; Export whitespaces in org files.
 (setq org-export-preserve-breaks "t")
 ;; Allow tab to work in embedded code blocks.
@@ -37,17 +39,21 @@
 ;; Org Capture Templates
 (setq org-capture-templates '(
     ("b" "Book Entry" entry (file+olp+datetree 
-			     "~/Notes/Org-Mode/Media.org"
-			     "Books") 
+			     "~/Notes/Media.org"
+			     "Books" "Read") 
      "* %^{Title} by %^{Author} %^g \n  %?")
-    ("j" "Journal Entry" plain (file+olp+datetree
-				"~/Notes/Org-Mode/Media.org"
-				"Journal")
-    "%?")
+    ("l" "Life TODO" entry (file+olp
+				"~/Notes/Life.org"
+				"Todos")
+     "* TODO %^{Title}")
+    ("t" "Work TODO" entry (file+olp
+				"~/Notes/Work.org"
+				"General" "Todos")
+    "* TODO %^{Title}")
 ))
 
 ;; Automatically load tangotango theme on startup.
-(load-theme 'tangotango t)
+(load-theme 'doom-one-light t)
 
 ;; Helm Config
 ;; Taken from https://tuhdo.github.io/helm-intro.html.
@@ -68,19 +74,21 @@
 
 (helm-mode 1)
 
-
-
+;; Overwrite highlighted region
+(delete-selection-mode 1)
 ;; Custom Set
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(adaptive-wrap)))
+ '(custom-safe-themes
+   '("e074be1c799b509f52870ee596a5977b519f6d269455b84ed998666cf6fc802a" "4bca89c1004e24981c840d3a32755bf859a6910c65b829d9441814000cf6c3d0" "77113617a0642d74767295c4408e17da3bfd9aa80aaa2b4eeb34680f6172d71a" "99ea831ca79a916f1bd789de366b639d09811501e8c092c85b2cb7d697777f93" default))
+ '(package-selected-packages '(org-gcal doom-themes adaptive-wrap)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(highlight ((t (:background "#fafafa" :foreground "#f0f0f0")))))
